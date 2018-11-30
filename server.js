373601235c8d96f31fbd640ed053f9ee302fc625
Nicolas Pestre <Nico@celines-MacBook-Air.local>
2018-11-30 14:32:40 +0100

    addall

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+const fs = require("fs");
+
+/*
+*
+* method >listUsers
+* method GET : lecture d'une resource 
+*/
+
+app.get('/listUsers', function(req, res){
+    fs.readFile(__dirname + "/" + "users.json", 'utf-8', function(err, data){
+        console.log(data);
+        res.send(data);
+    });
+})
+
+let user = {
+	"user4" : {
+		"name" : "mohit",
+		"password" : "password4",
+		"profession" : "teacher",
+		"id": 4
+	}
+}
+/* 
+*
+*
+* method >addUsers
+* method POST : creation d'une resource 
+*
+*
+*/
+
+app.post('/addUsers', function(req, res) {
+	fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+		data = JSON.parse(data);
+		data["user4"] = user["user4"];
+		console.log(data);
+        res.send(JSON.stringify(data));
+    });
+})
+
+/*
+*
+* method > getUserByID
+* method Get : Recuperer la ressource d'un utilisateur par son identifiant 
+*
+*/
+
+app.get('/getUserByID', function(req, res) {
+	fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+	if(err) throw err;
+		//Variable 'user' wiil contain the result of users information to retrieve all ressources
+		let users = JSON.parse(data); 
+		//Variable 'user' retrieve user information by id 
+		let user = users["user" + req.params.id]
+		//Display 'user' information by id 
+		console.log("User Info by id: " + JSON.stringify(user));
+		res.send(JSON.stringify(user));
+	});
+})
+
+
+
+let server = app.listen(8081, function(){
+    let host = server.address().address
+    let port = server.address().port
+    console.log("Launch API restful with the following URI http://%s:%s", host, port)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
